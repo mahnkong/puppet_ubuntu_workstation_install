17cea98ce87835258c3421cc5c428ee57aa462be
mahnkong <mahnkong@gmx.de>
2018-04-30 08:34:02 +0200

    keepass2

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [[ $EUID > 0 ]] && [ "$SUDO_USER" = "" ]; then
+if [[ $EUID > 0 ]] || [ "$SUDO_USER" = "" ]; then
     echo "Please run as sudo"
     exit 1
 fi
 
+FORCE_PUPPET_INSTALL="$1"
 export FACTER_BUILDDIR="$(pwd)/build"
 export FACTER_BUILDUSER=$SUDO_USER
 export FACTER_BUILDUSERHOME=$(echo $(getent passwd $SUDO_USER )| cut -d : -f 6)
@@ -13,7 +14,7 @@ mkdir -p $FACTER_BUILDDIR
 chown $FACTER_BUILDUSER:$FACTER_BUILDUSER $FACTER_BUILDDIR
 
 apt update
-if ! which puppet >/dev/null  || [ "$FORCE_PUPPET_INSTALL" != "" ]; then
+if [ "$(which puppet 2>/dev/null)" = "" -o "$FORCE_PUPPET_INSTALL" != "" ]; then
     apt install -y puppet
 fi
 puppet apply --modulepath ./modules manifests/site.pp
--- modules/puppet_workstation_install/manifests/keepass2.pp
+++ modules/puppet_workstation_install/manifests/keepass2.pp
@@ -1,7 +1,12 @@
 class puppet_workstation_install::keepass2 {
 
   package {'mono-complete': ensure  => "installed" }
+  package {'libmono-system-xml-linq4.0-cil': ensure  => "installed" }
+  package {'libmono-system-data-datasetextensions4.0-cil': ensure  => "installed" }
+  package {'libmono-system-runtime-serialization4.0-cil': ensure  => "installed" }
+  package {'mono-mcs': ensure  => "installed" }
   package {'keepass2': ensure  => "installed" }
+
 
   file {"${::builduserhome}/.config/KeePass":
     owner  => $::builduser,
@@ -14,6 +19,17 @@ class puppet_workstation_install::keepass2 {
     owner   => $::builduser,
     group   => $::builduser,
     content => template('puppet_workstation_install/keepass2/KeePass.config.xml.erb')
+  }
+
+  file {"${::builddir}/install_keepasshttp.sh": 
+    ensure => present,
+    content => template("puppet_workstation_install/keepass2/install_keepasshttp.sh.erb"),
+    notify => Exec["/bin/bash ${::builddir}/install_keepasshttp.sh"]
+  }
+
+  exec {"/bin/bash ${::builddir}/install_keepasshttp.sh":
+    refreshonly => true,
+    require     => Package['keepass2','curl']
   }
 
 }
